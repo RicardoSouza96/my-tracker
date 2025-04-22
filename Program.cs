02503aeb975766a884467c9f5624a376295d43ae
Ricardo Souza <ricardoalmeidasouza96@gmail.com>
2025-04-22 20:47:29 -0300

    Adjusts the connection string to the database and configure the initial seeding of the sample data

--- Program.cs
+++ Program.cs
@@ -2,15 +2,38 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using my_tracker.Models;
 using my_tracker.Data;
+using System;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlite(builder.Configuration.GetConnectionString("MyTrackerContext")));
+{
+    var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+    var databasePath = Path.Combine(Directory.GetCurrentDirectory(), connectionString.Split('=')[1]);
+
+    Directory.CreateDirectory(Path.GetDirectoryName(databasePath));
+    options.UseSqlite($"Data Source={databasePath}");
+});
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    try
+    {
+        var context = services.GetRequiredService<ApplicationDbContext>();
+        context.Database.EnsureCreated();
+        DbInitializer.Initialize(context);
+    }
+    catch (Exception ex)
+    {
+        var logger = services.GetRequiredService<ILogger<Program>>();
+        logger.LogError(ex, "Ocorreu um erro ao inicializar o banco de dados");
+    }
+}
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
