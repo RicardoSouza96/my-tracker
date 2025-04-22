197c0774e7f2435b841d936d5d340ac7dfa5a980
Ricardo Souza <ricardoalmeidasouza96@gmail.com>
2025-04-22 20:34:37 -0300

    Changes the Controller to return the list of Mangas in the View

--- Controllers/MangaController.cs
+++ Controllers/MangaController.cs
@@ -1,12 +1,20 @@
 using my_tracker.Models;
 using Microsoft.AspNetCore.Mvc;
+using my_tracker.Data;
 
 namespace my_tracker.Controllers;
 
 public class MangaController : Controller
 {
+    private readonly ApplicationDbContext _context;
+
+    public MangaController(ApplicationDbContext context)
+    {
+        _context = context;
+    }
     public IActionResult Index()
     {
-        return View();
+        var mangas = _context.Mangas.ToList();
+        return View(mangas);
     }
 }
