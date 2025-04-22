5a0a96af3a466fe378b10c9decb7a6b52c6157ca
Ricardo Souza <ricardoalmeidasouza96@gmail.com>
2025-04-22 20:36:44 -0300

    Creates the database model with the Mangas, Animes and Games tables

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -5,5 +5,16 @@ namespace my_tracker.Data;
 
 public class ApplicationDbContext : DbContext
 {
+    public DbSet<Manga> Mangas{ get; set; }
+    public DbSet<Anime> Animes { get; set; }
+    public DbSet<Game> Games { get; set; }
+
     public ApplicationDbContext(DbContextOptions options) : base(options) {}
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Manga>().ToTable("Mangas");
+        modelBuilder.Entity<Anime>().ToTable("Animes");
+        modelBuilder.Entity<Game>().ToTable("Games");
+    }
 }
